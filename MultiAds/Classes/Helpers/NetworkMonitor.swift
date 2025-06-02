48632542a87d6574c594062c3ce7a801164b3ac7
Bukhari Rehan Abbas <44895678+CoderRb123@users.noreply.github.com>
2025-06-02 21:28:40 +0530

    Check Network

--- MultiAds/Classes/Helpers/NetworkMonitor.swift
+++ MultiAds/Classes/Helpers/NetworkMonitor.swift
@@ -1,10 +1,3 @@
-//
-//  NetworkMonitor.swift
-//  MultiAdsInterface
-//
-//  Created by Khusnud Zehra on 03/05/25.
-//
-
 import Foundation
 import Network
 import Combine
@@ -13,18 +6,42 @@ import Combine
 final class NetworkMonitor: ObservableObject {
     
     static var shared: NetworkMonitor = NetworkMonitor()
+    
     private let networkMonitor = NWPathMonitor()
     private let workerQueue = DispatchQueue(label: "Monitor")
-
+    
     @Published var isConnected: Bool = false
-
+    
     init() {
         networkMonitor.pathUpdateHandler = { [weak self] path in
-            DispatchQueue.main.async {
-                self?.isConnected = path.status == .satisfied
+            guard path.status == .satisfied else {
+                DispatchQueue.main.async {
+                    self?.isConnected = false
+                }
+                return
             }
+            
+            // Network available, now check actual internet access
+            self?.checkInternetAccess()
         }
         networkMonitor.start(queue: workerQueue)
+    }
+    
+    private func checkInternetAccess() {
+        guard let url = URL(string: "https://www.apple.com/library/test/success.html") else { return }
+        
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 5 // short timeout for responsiveness
+        
+        URLSession.shared.dataTask(with: request) { _, response, _ in
+            DispatchQueue.main.async {
+                if let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) {
+                    self.isConnected = true
+                } else {
+                    self.isConnected = false
+                }
+            }
+        }.resume()
     }
 
     deinit {
